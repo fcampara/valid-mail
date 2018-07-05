f2fadbf8182b6561019d7d3235ed910f8b9c3482
Felipebr26 <fcamparasilva@gmail.com>
2018-07-05 09:53:30 -0300

    nodejs

--- api/config/app.conf.js
+++ api/config/app.conf.js
@@ -1,19 +1,22 @@
-const express = require('express'),
-  app = module.exports = express(),
-  path = require('path'),
+const
+  express = require('express'),
   serveStatic = require('serve-static'),
   bodyParser = require('body-parser'),
   history = require('connect-history-api-fallback'),
+  path = require('path')
+
+const
   allowCors = (req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:8080, https://valid-mail.herokuapp.com, http://valid-mail.herokuapp.com/')
     res.header('Access-Control-Allow-Methods', 'GET')
     res.header('Access-Control-Allow-Headers', 'Content-Type')
     res.header('Access-Control-Allow-Credentials', 'true')
-
     next()
-  }
+  },
+  port = process.env.PORT || 5000,
+  app = module.exports = express()
 
-app.listen(5000)
+app.listen(port)
 app.use(allowCors)
 app.use(bodyParser.json({
   limit: '50mb'
