d40d6dbbb69061d2963e9372c0f79b2feceb7bbb
Felipebr26 <fcamparasilva@gmail.com>
2018-07-05 09:05:47 -0300

    Firebase config

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -1,6 +1,6 @@
 import Firebase from 'firebase/app'
 import 'firebase/firestore' // eslint-disable-line
-import firebaseConfig from '../../firebase.conf'
+import firebaseConfig from '../../firebase.conf.js'
 
 export default ({ Vue }) => {
   // Initialize Firebase from settings
